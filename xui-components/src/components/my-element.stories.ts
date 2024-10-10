63d5c59a14f6222fcb5821ef77d70599896be5ea
Magali Bautista-Lee <magzb.99@gmail.com>
2024-10-10 15:10:42 -0400

    chore: Displays test Lit component in Storybook. Removes starter code. (#5)

---
+++ xui-components/src/components/my-element.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { MyElement } from './my-element';
+import './my-element';
+
+const meta: Meta<typeof MyElement> = {
+  title: 'Example/MyElement',
+  component: 'my-element',
+  render: () => html`<my-element>
+    <h1>Vite + Lit</h1>
+  </my-element>`,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MyElement>;
+
+export const Primary: Story = {};
---
+++ xui-components/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // If we want to publish standalone components we don't externalize lit,
+      // if you are going to use lit in your own project, you can make it a dep instead.
+      // external: /^lit/, <-- comment this line
+    },
+  },
+});
