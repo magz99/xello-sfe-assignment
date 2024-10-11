eee8c13ccc1800992e495dda212b96243acb9bf9
magz99 <magzb.99@gmail.com>
2024-10-10 20:33:26 -0400

    feat: Adds support for enabling and disabling trigger button in Storybook. Creates Custom Elements JSON for typing issues

--- xui-components/src/components/reaction/trigger-button/trigger-button.ts
+++ xui-components/src/components/reaction/trigger-button/trigger-button.ts
@@ -1,9 +1,10 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { triggerIcon } from './icons';
 
 @customElement('xui-reaction-trigger-button')
 export class ReactionTriggerButton extends LitElement {
+  // TODO: Some of the colours should be css vars for theming.
   static styles = css`
     button {
       height: 32px;
@@ -18,20 +19,39 @@ export class ReactionTriggerButton extends LitElement {
 
       color: #484848;
     }
-    button:hover {
-      cursor: pointer;
-      color: #027baf;
+    button:not(:disabled) {
+      &:hover {
+        cursor: pointer;
+        color: #027baf;
+      }
+    }
+
+    button:disabled {
+      color: var(--xui-reaction-trigger-disabled, #828282);
     }
   `;
 
   @property({ type: String })
   ariaLabel: string = 'Select a reaction';
 
-  @property({ type: Boolean, reflect: true })
-  disabled = false;
+  @property({
+    type: Boolean,
+    converter: (value, type) => {
+      // `value` is a string
+      // Convert it to a value of type `type` and return it
+      console.log('button: value and type: ', value, type);
+      console.log('button typeof value: ', typeof value);
+      return value === 'true';
+    },
+  })
+  isDisabled: boolean = false;
 
   render() {
-    return html`<button part="button" aria-label=${this.ariaLabel}>
+    return html`<button
+      ?disabled=${this.isDisabled}
+      part="button"
+      aria-label=${this.ariaLabel}
+    >
       ${triggerIcon}
     </button>`;
   }
