fc7263b65f413be257a409c39668e8e569891963
magz99 <magzb.99@gmail.com>
2024-10-15 12:16:59 -0400

    chore: Cleanup and add readme

--- xui-components/vite.config.ts
+++ xui-components/vite.config.ts
@@ -8,10 +8,5 @@ export default defineConfig({
       entry: 'src/index.ts',
       formats: ['es'],
     },
-    rollupOptions: {
-      // If we want to publish standalone components we don't externalize lit,
-      // if you are going to use lit in your own project, you can make it a dep instead.
-      // external: /^lit/, <-- comment this line
-    },
   },
 });
